6ec774bdc87afbaa3752b33e583ab9f5e8f82b56
Xiaozao Wang <115412906+XiaozaoWang@users.noreply.github.com>
2025-12-16 20:33:42 +0800

    fix route

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,7 +5,7 @@ export const baseUrl = "https://portfolio-blog-starter.vercel.app";
 export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
     url: `${baseUrl}/work/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
+    lastModified: new Date().toISOString().split("T")[0],
   }));
 
   let routes = ["", "/work"].map((route) => ({
